339a9b7612232454323fed0c2bc2b340d96df44e
Everton Cardoso Acchetta <eve.023@hotmail.com>
2018-02-28 17:05:14 -0300

    Add files via upload

---
+++ Codes/Ex1 Aula1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, produto;
+    cout << "Entre o valor dos 3 numeros desejados: ";
+    cin >> a >> b >> c;
+    produto = a * b * c;
+    cout << "O produto dos 3 numeros é: " << produto << endl;
+
+    return 0;
+
+}
